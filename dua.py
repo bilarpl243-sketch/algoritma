4c212af4fa3b151d659715d16ae40d8ce218a3e7
bilarpl243-sketch <306969222+bilarpl243-sketch@users.noreply.github.com>
2026-08-26 08:10:06 +0700

    Add files via upload

---
+++ dua.py
@@ -0,0 +1,12 @@
+# program pertamaku
+# nama: bila 
+# kelas: X RPL
+
+print ("=========================================")
+print ("     SELAMAT DATANG DI PHYTHON!")
+print ("=========================================")
+print ()
+print ("progam ini dibuat oleh:","(bila)")
+print ("sekolah: SMK TJP TUBAN")
+print ("Tahun :", 2024)
+print
