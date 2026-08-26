0f2da8bb3a799ec01d0592e61b7cc8a48694d408
bilarpl243-sketch <306969222+bilarpl243-sketch@users.noreply.github.com>
2026-08-26 07:32:59 +0700

    Add files via upload

---
+++ tiga.py
@@ -0,0 +1,15 @@
+nama = "bila"
+print("nama : ", bila)
+umur = 15
+print("umur = ", umur)
+tinggi = 165 cm
+print("tinggi : ", tinggi, cm)
+lulus = false
+print("lulus :", lulus)
+
+nama produk: "sepatu lari"
+print("nama produk :", sepatu lari)
+stok tersedia : 15
+print("stok tersedia :", stok tersedia)
+harga ($) : 249,5
+print("")
